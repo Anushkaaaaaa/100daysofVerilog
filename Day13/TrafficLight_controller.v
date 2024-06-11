3d33d28f6e5d0b4d1334f768d4b35414c7b8d284
Anushka Srivastava <99269388+Anushkaaaaaa@users.noreply.github.com>
2024-06-11 20:17:12 +0800

    Add files via upload

---
+++ Day13/TrafficLight_controller.v
@@ -0,0 +1,35 @@
+module TrafficLight_controller(
+	input clk,
+	output reg red,yellow,green
+	
+);
+
+parameter ST_INIT = 2'b00, ST_GREEN = 2'b01, ST_YELLOW = 2'b10, ST_RED = 2'b11;
+
+
+
+reg [2:0] state = ST_INIT;
+
+always @(posedge clk) begin
+	case (state)
+		ST_INIT: begin
+			state = ST_GREEN; red =0;
+				yellow =0; green = 0;
+		end
+		ST_GREEN: begin
+			state = ST_YELLOW; red =0;
+				yellow =0; green = 1;
+		end
+		ST_YELLOW: begin
+			state = ST_RED; red =0;
+				yellow =1; green = 0;
+		end
+		ST_RED: begin
+			state = ST_GREEN; red =1;
+				yellow =0; green = 0;
+		end
+		default : state = ST_INIT;
+	endcase
+end
+
+endmodule
